fd34ab6c0559302d441b160129be1eb3a41f9b31
Kidami13 <ulyanaboo1407@gmail.com>
2020-10-31 12:51:43 +0200

    HW_7.cpp

---
+++ HW_7.cpp
@@ -0,0 +1,34 @@
+﻿//
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+/*************************************************Домашка №1******************************************************/
+    int numbers[5];
+    cout << "input five numbers" << endl;
+    for (int i = 0;i < 5;i++) {
+        cout << "input number " <<i+1<< endl;
+        cin >> numbers[i];
+       
+    }
+    for (int i = 0;i < 5;i++) {
+        
+        cout << numbers[i]<< " ";
+
+    }
+    cout << endl;
+/*************************************************Домашка №2******************************************************/
+    char word[10];
+    cout << "input word" << endl;
+    cin >> word;
+    int i = 0;
+    while (word[i] != 0) {
+        cout << word[i] << endl;
+        i++;
+    }
+
+    
+}
+
